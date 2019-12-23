c1f3fba579bcfe8af999864dc337e214a624d466
Karolina Mizera <mizera1112@gmail.com>
2019-12-23 22:22:08 +0100

    path fix

--- static/script.js
+++ static/script.js
@@ -1,4 +1,4 @@
-var serverPath = "https://hobby-oghlklmkakojgbkepalfbfdl.dbs.graphenedb.com:24780/db/data/"// "http://localhost:8080";
+var serverPath = "https://hobby-oghlklmkakojgbkepalfbfdl.dbs.graphenedb.com:24780/db/data"// "http://localhost:8080";
 
 function getRequestObject() {
     if ( window.ActiveXObject) {
